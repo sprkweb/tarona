bfc5e718d32a626616effd5cd2d470223c942658
Vadim Saprykin <sprkweb@ya.ru>
2016-08-20 11:59:37 +0300

    WebSocket

--- lib/tarona/web_socket.rb
+++ lib/tarona/web_socket.rb
@@ -2,5 +2,34 @@ module Tarona
   # Instances of this class are event-driven WebSocket connections using the
   # GameIO interface.
   class WebSocket < Tardvig::GameIO
+    def self.player?(env)
+      Faye::WebSocket.websocket? env
+    end
+
+    def initialize(env)
+      @socket = Faye::WebSocket.new env
+      @socket.on :message, &method(:input2event)
+    end
+
+    def happen(event, data)
+      event2output(event, data)
+      super(event, data)
+    end
+
+    alias trigger happen
+
+    private
+
+    def event2output(event, data)
+      @socket.send JSON.dump([event, data])
+    end
+
+    def input2event(event)
+      input = JSON.parse event.data
+      return if input.size != 2 || input[0].class != String
+      listeners(input[0].to_sym).each do |listener|
+        listener.call input[1]
+      end
+    end
   end
 end
---
+++ spec/websocket_spec.rb
@@ -0,0 +1,50 @@
+RSpec.describe Tarona::WebSocket do
+  let(:env) { double }
+
+  it 'is implementation of Tardvig::GameIO' do
+    expect(described_class.ancestors).to include(Tardvig::GameIO)
+  end
+
+  describe '#player?' do
+    it 'checks whether it is websocket connection' do
+      boolean = double
+      expect(Faye::WebSocket).to receive(:websocket?).with(env) { boolean }
+      expect(described_class.player?(env)).to be(boolean)
+    end
+  end
+
+  it 'is event-driven wrapper of Faye::WebSocket' do
+    expect(Faye::WebSocket).to receive(:new).with(env) { spy }
+    described_class.new env
+  end
+
+  let(:io) { described_class.new env }
+  let(:socket) { double }
+  before(:example) do
+    allow(socket).to receive(:listeners) { @listeners }
+    allow(socket).to receive(:on) do |event, &block|
+      @listeners ||= []
+      @listeners << [event, block]
+    end
+    allow(Faye::WebSocket).to receive(:new) { socket }
+  end
+
+  it 'converts output events to JSON' do
+    expect(socket).to receive(:send) do |args|
+      expect(JSON.parse(args)).to match_array(['event', 'args'])
+    end
+    io.happen :event, 'args'
+  end
+
+  it 'converts input JSON to events' do
+    listener = proc {}
+    io.on :event, &listener
+    expect(listener).to receive(:call).with('args')
+    event = double
+    expect(event).to receive(:data) { JSON.dump [:event, 'args'] }
+    socket.listeners.each do |listener|
+      next unless listener[0] == :message
+      listener[1].call event
+    end
+  end
+end
