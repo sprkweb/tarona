3b9f42893bba7507920a626d4fc911ceaac3f815
Vadim Saprykin <sprkweb@ya.ru>
2016-10-10 18:53:16 +0300

    Action description

--- lib/tarona/act_types/action.rb
+++ lib/tarona/act_types/action.rb
@@ -1,4 +1,7 @@
 module Tarona
+  # Action is a main type of act. In this act players control the game course.
+  # It consists of a map. Player controls things which belongs to him on the
+  # map (these things on the map are called "entities").
   class Action < Act
     act_type :action
   end
