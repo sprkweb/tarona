40dce62f03281e9968f7c077a758c51e7556e1ea
Vadim Saprykin <sprkweb@ya.ru>
2016-10-13 18:30:32 +0300

    TextAct docs

--- lib/tarona/act_types/text_act.rb
+++ lib/tarona/act_types/text_act.rb
@@ -1,6 +1,9 @@
 module Tarona
   # This class represents part of a game. It consists of text, like the
   # beginning of "Star Wars".
+  #
+  # To create it, you just need to set `subject` and {#next_act}.
+  # You should not overwrite the {#process} method.
   class TextAct < Act
     act_type :text
 
@@ -10,8 +13,9 @@ module Tarona
       end
     end
 
-    private
-
+    # You should redefine it.
+    # @return [Tarona::Act] next act. It will be started as soon as player
+    #   decide to continue.
     def next_act
     end
   end
