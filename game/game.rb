175cf275327204966446523ee0a04a61d479d59f
Vadim Saprykin <sprkweb@ya.ru>
2017-09-05 18:27:54 +0300

    Game automatically opens in default browser

--- game/game.rb
+++ game/game.rb
@@ -19,31 +19,3 @@ require 'game/acts/introduction'
 require 'game/acts/placeholder'
 require 'game/acts/the_end'
 require 'game/pages'
-
-toolkit = Tarona::Toolkit
-Tarona::WebServer.tk = toolkit.new
-doorman = Tarona::Doorman.new(
-  io: Tarona::WebSocket,
-  server: Tarona::WebServer,
-  game: Tarona::Play,
-  game_options: {
-    acts: {
-      intro: Tarona::Introduction,
-      pholder: Tarona::Placeholder,
-      the_end: Tarona::TheEnd
-    },
-    first_act: :intro,
-    tk: toolkit
-  }
-)
-
-unless ENV['RACK_ENV'] == 'test'
-  ENV['RACK_ENV'] = 'production'
-  rack_options = {
-    app: doorman,
-    server: :puma
-  }
-  rack_options[:Port] = ENV['PORT'] if ENV['PORT']
-  rack_options[:Host] = ENV['IP'] if ENV['IP']
-  Rack::Server.start rack_options
-end
