ff6ad09e388499e54cc0d6e5e88dac376f0ea7e1
Vadim Saprykin <sprkweb@ya.ru>
2017-09-12 21:04:48 +0300

    Fix https://travis-ci.org/sprkweb/tarona/builds/273347317

--- public/scripts/new_game.js
+++ public/scripts/new_game.js
@@ -1,5 +1,13 @@
-(function() {
+var NewGame = function() {
   var expires = (new Date('1')).toUTCString();
   document.cookie = 'session_id=; expires: ' + expires;
   location.href = './play';
-})();
+};
+
+if (typeof environment === 'undefined') {
+  var environment = 'production';
+}
+
+if (environment !== 'test') {
+  NewGame();
+}
