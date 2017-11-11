fc0a3d79b39a1656c0f81b1a362f8ab703a290e4
Vadim Saprykin <sprkweb@ya.ru>
2017-11-11 19:28:52 +0300

    Training level: add soldiers of radls

--- game/acts/training.rb
+++ game/acts/training.rb
@@ -8,22 +8,65 @@ module Tarona
 
     def process
       io.on_first :map_shown do
-        Game::ViewScripts.run io, <<-SCRIPT.gsub(/^ {10}/, '')
-          var showText = function(text) {
-            var win = new PopUp(env.area, text, { stick_to: 'top-right'});
-            win.show();
-          };
+        show_controls_info
+        when_he_shot_crystal do
+          add_radls
+        end
+      end
+    end
 
-          showText('#{tk.i18n['game/training_fov'].chomp}');
-          showText('#{tk.i18n['game/training_choose'].chomp}');
+    def show_controls_info
+      Game::ViewScripts.run io, <<-SCRIPT.gsub(/^ {10}/, '')
+        var openedWins = [];
+        var showText = function(text) {
+          var win = new PopUp(env.area, text, { stick_to: 'top-right'});
+          win.show();
+          openedWins.push(win);
+        };
+        var clearPopups = function() {
+          openedWins.forEach(function(w) { w.close() });
+        };
 
-          var heroSelected = function(ev) {
-            if (ev.now.id == 'hero')
-              showText('#{tk.i18n['game/training_interact'].chomp}');
-            essence.remove_listener('focusChange', heroSelected)
-          };
-          essence.on('focusChange', heroSelected);
-        SCRIPT
+        showText('#{tk.i18n['game/training_fov'].chomp}');
+        showText('#{tk.i18n['game/training_choose'].chomp}');
+
+        var heroSelected = function(ev) {
+          clearPopups();
+          if (ev.now.id == 'hero')
+            showText('#{tk.i18n['game/training_interact'].chomp}');
+          essence.remove_listener('focusChange', heroSelected)
+        };
+        essence.on('focusChange', heroSelected);
+      SCRIPT
+    end
+
+    def when_he_shot_crystal(&block)
+      judge = rules.interactions_judge
+      after_shot = proc do |ev|
+        if he_shot_crystal?(ev)
+          judge.remove_listener(:after_interact, &after_shot)
+          block.call
+        end
+      end
+      judge.on(:after_interact, &after_shot)
+    end
+
+    def he_shot_crystal?(ev)
+      ev[:from].id == 'hero' && ev[:to].id.start_with?('crystal') &&
+        ev[:interaction] == ev[:from].interactions['lazer_rifle_shoot']
+    end
+
+    def add_radls
+      act_inf = @tk.session[:act_inf]
+      radl_class = Game::Templates::RadlSoldier
+      list = [['1', [52, 22]], ['2', [57, 28]]]
+      list.each do |inf|
+        obj = radl_class.new(
+          "radl_soldier#{inf[0]}", radl_class::TEMPLATE, hp: radl_class::MAX_HP
+        )
+        Action::PlaceEntity.add(act_inf[:landscape], obj, inf[1])
+        act_inf[:entities_index][obj.id] = inf[1]
+        io.happen :add_entity, entity_inf: obj.raw, place: inf[1]
       end
     end
   end
--- game/classes/entities/templates.rb
+++ game/classes/entities/templates.rb
@@ -2,10 +2,9 @@ module Tarona
   module Game
     # Classes with preset properties for some type of entities.
     module Templates
-      class DidorianSoldier < Tarona::Game::Fighter
-        def template() 'soldier' end
-        def name() 'people/didorian_soldier' end
-        def side() 'player' end
+      class AbstractSoldier < Tarona::Game::Fighter
+        MAX_HP = 100
+
         def regen_energy() 200 end
         def max_energy() 600 end
         def max_hp() 100 end
@@ -13,7 +12,7 @@ module Tarona
         def move_speed() 16 end
 
         def tags
-          []
+          [:mortal]
         end
 
         def hexes
@@ -22,6 +21,26 @@ module Tarona
             odd_row: [[0, 0], [0, 1], [1, 1]]
           }
         end
+      end
+
+      class DidorianSoldier < AbstractSoldier
+        TEMPLATE = 'soldier'.freeze
+
+        def template() 'soldier' end
+        def name() 'people/didorian_soldier' end
+        def side() 'player' end
+      end
+
+      class RadlSoldier < AbstractSoldier
+        TEMPLATE = 'soldier'.freeze
+        MAX_HP = 150
+
+        def template() 'enemy_soldier' end
+        def name() 'people/radl_soldier' end
+        def side() 'radls' end
+        def max_energy() 800 end
+        def max_hp() 150 end
+        def move_speed() 12 end
       end
     end
   end
